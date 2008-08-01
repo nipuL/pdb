559d785d32aa047abaec6a3ae33a84904cd633d5
Lucas Hazel <lucas@die.net.au>
2008-08-01 23:40:08 +1000

    add query module

---
+++ query.py
@@ -0,0 +1,34 @@
+import urllib, urllib2
+
+from portdb import *
+from xmlobj import *
+
+class Request(object):
+    def __init__(self, url, parameters):
+        self.parameters = {'f':'xml'}
+        self.parameters.update(parameters)
+        self.url = url
+
+    def read(self):
+        return urllib2.urlopen('?'.join((
+                    self.url,
+                    urllib.urlencode(self.parameters)
+                    ))).read()
+
+class Index(Request):
+    def __init__(self, url):
+        super(Index, self).__init__(url, {'a':'index'})
+
+class Repo(Request):
+    def __init__(self, url, repo):
+        super(Repo, self).__init__(url, {'a':'repo', 'q':repo})
+
+class Search(Request):
+    def __init__(self, url, query, strict=False):
+        super(Search, self).__init__(url, {'a':'search', 'q':query})
+        if strict:
+            self.parameters.update({'strict':'true'})
+
+if __name__ == '__main__':
+    url = 'http://crux.nu/~lucas/portdb/'
+    print Search(url,'port').read()
